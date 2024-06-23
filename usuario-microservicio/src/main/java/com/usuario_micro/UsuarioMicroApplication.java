1bb1c9c0303ee24c0a1478664f746d9364f3f68b
Christian <christiangarciavazquez11@gmail.com>
2024-06-23 16:40:13 -0600

    default zone

--- usuario-microservicio/src/main/java/com/usuario_micro/UsuarioMicroApplication.java
+++ usuario-microservicio/src/main/java/com/usuario_micro/UsuarioMicroApplication.java
@@ -1,5 +1,6 @@
 package com.usuario_micro;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
