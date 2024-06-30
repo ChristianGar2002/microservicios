29ec7f2ea6d7e3304f99c1340ca2f060f4ad37fb
Christian <christiangarciavazquez11@gmail.com>
2024-06-30 16:43:58 -0600

    CB

--- usuario-microservicio/src/main/java/com/usuario_micro/controlador/UsuarioControlador.java
+++ usuario-microservicio/src/main/java/com/usuario_micro/controlador/UsuarioControlador.java
@@ -4,7 +4,10 @@ import com.usuario_micro.entidades.Usuario;
 import com.usuario_micro.modelos.Carro;
 import com.usuario_micro.modelos.Moto;
 import com.usuario_micro.servicio.UsuarioServicio;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -51,7 +54,9 @@ public class UsuarioControlador {
 
         return ResponseEntity.ok(nuevoUsuario);
     }
-
+    //Cuando susceda un error al llamar a este microservicio se llama al metodo fallBackGetCarros para que le un mensaje devuelta
+    //@CicuitBreaker sirve para evitar que se ciagan todos los microservicios, name="Al nombre que esta en bootstrap.yaml es la instancia con la que manejaremos la tolerancia de fallos, el fallback es el nombre del metodo que creare"
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackGetCarros")
     @GetMapping("/carros/{usuarioId}")
     public ResponseEntity<List<Carro>> listarCarros(@PathVariable("usuarioId") int id){
 
@@ -67,6 +72,7 @@ public class UsuarioControlador {
         return ResponseEntity.ok(carros);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGeMotos")
     @GetMapping("/motos/{usuarioId}")
     public ResponseEntity<List<Moto>> listarMotos(@PathVariable("usuarioId") int id){
 
@@ -82,6 +88,7 @@ public class UsuarioControlador {
         return ResponseEntity.ok(motos);
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackSaveCarro")
     @PostMapping("/carro/{usuarioId}")
     public ResponseEntity<Carro> guardarCarro(@PathVariable("usuarioId") int usuarioId, @RequestBody Carro carro){
 
@@ -90,6 +97,7 @@ public class UsuarioControlador {
         return ResponseEntity.ok(nuevoCarro);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGetMoto")
     @PostMapping("/moto/{usuarioId}")
     public ResponseEntity<Moto> guardarMoto(@PathVariable("usuarioId") int usuarioId, @RequestBody Moto moto){
 
@@ -98,6 +106,7 @@ public class UsuarioControlador {
         return ResponseEntity.ok(nuevaMoto);
     }
 
+    @CircuitBreaker(name = "todosCB", fallbackMethod = "fallBackGetTodos")
     @GetMapping("/todos/{usuarioId}")
     public ResponseEntity<Map<String, Object>> listarTodosLosVehiculos(@PathVariable("usuarioId") int usuarioId){
 
@@ -105,5 +114,30 @@ public class UsuarioControlador {
 
         return ResponseEntity.ok(resultado);
 
+    }
+
+    private ResponseEntity<List<Carro>> fallBackGetCarros(@PathVariable("usuarioId") int id, RuntimeException exception){//Si no se puede acceder al microservicio carro sucedaedara esto
+
+        return new ResponseEntity("El usuario: " + id + " tiene los carros en el taller", HttpStatus.ACCEPTED);
+    }
+
+    private ResponseEntity<Carro> fallBackSaveCarro(@PathVariable("usuarioId") int id, @RequestBody Carro carro, RuntimeException exception){//Si no se puede acceder al microservicio carro sucedaedara esto
+
+        return new ResponseEntity("El usuario: " + id + " no tiene dinero para los carros", HttpStatus.ACCEPTED);
+    }
+
+    private ResponseEntity<List<Moto>> fallBackGetMotos(@PathVariable("usuarioId") int id, RuntimeException exception){//Si no se puede acceder al microservicio carro sucedaedara esto
+
+        return new ResponseEntity("El usuario: " + id + " tiene las motos en el taller", HttpStatus.ACCEPTED);
+    }
+
+    private ResponseEntity<Moto> fallBackSaveMoto(@PathVariable("usuarioId") int id, @RequestBody Moto moto, RuntimeException exception){//Si no se puede acceder al microservicio carro sucedaedara esto
+
+        return new ResponseEntity("El usuario: " + id + " no tiene dinero para los carros", HttpStatus.ACCEPTED);
+    }
+
+    private ResponseEntity<Map<String, Object>> fallBackGetTodos(@PathVariable("usuarioId") int id, RuntimeException exception){//Si no se puede acceder al microservicio carro sucedaedara esto
+
+        return new ResponseEntity("El usuario: " + id + " tiene los vehiculos en el taller", HttpStatus.ACCEPTED);
     }
 }
